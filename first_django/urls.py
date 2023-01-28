88057f0821dbd1c10f77ab24f31951217d86b884
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-28 13:53:10 +0200

    brif import

--- first_django/urls.py
+++ first_django/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
-from pages.views import contact_view
-from pages.views import about_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('home/', home_view, name='home'),
