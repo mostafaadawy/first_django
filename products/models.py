9622fafa939621edf6a33935006a0fdca4215392
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-28 11:05:35 +0200

    adding fields to product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Product(models.Model):
+    title       = models.TextField()
+    description = models.TextField()
+    price       = models.TextField()
