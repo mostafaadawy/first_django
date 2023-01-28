bd4f6fb1f0adc6aa0e7dbc9b86cae3bcd5619e98
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-28 14:11:02 +0200

    using  templates

--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,9 @@ def home_view(request, *args, **kwargs):
     print('args', args)
     print('kwargs', kwargs)
     print('request', request.user)
-    return HttpResponse("<h1>Home Page</h1>")
+    # return HttpResponse("<h1>Home Page</h1>")
+    # passing the request, file to be rendered and data
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
